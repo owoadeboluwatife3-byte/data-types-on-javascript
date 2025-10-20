d69e8e8559f0b0fb58c5b07f44e3a21ecc610d62
Adams <owoadeboluwatife3@gmail.com>
2025-10-20 12:05:59 +0100

    update

---
+++ index.js
@@ -0,0 +1,77 @@
+// Data Types
+// - strings
+// - numbers
+// -booleans
+// -undefined
+// -null
+// -Symbol
+// -BigInt
+
+const myName = 'Adams'
+console.log (myName)
+const anotherName = 'Lildamz'
+console.log (myName)
+doublenames = myName + ' ' + anotherName
+console.log(doublenames)
+const num1 = '10'
+console.log(myName + num1)
+
+isStudent = false
+console.log(isStudent)
+let fruit = 'mango'
+console.log (fruit)
+
+const language = 'Javascript'
+const difficultylevel = '8'
+const isFun = true
+let nextlesson;
+const completedlesson = null
+console.log (language)
+console.log (difficultylevel)
+console.log (isFun)
+console.log (nextlesson)
+console.log(completedlesson)
+const sentence = language + ' ' +'is fun to learn'
+console.log(sentence)
+
+const sentence1 = `${language} is very fun to learn`
+console.log(sentence1)
+console.log(sentence)
+
+const a = 10
+const b = 5
+
+console.log(a + b)
+console.log(a - b)
+console.log(a * b)
+console.log(a / b)
+console.log(a % b)
+console.log(a ** b)
+
+//strings
+
+const _name1 = 'Joey'
+name1Upper = _name1.toUpperCase ()
+console.log(name1Upper)
+
+const name3 = 'Samuel'
+
+const slicedName = name3.slice(0,3)
+console.log(slicedName)
+
+const includesY = name3.includes('y')
+console.log(includesY)
+
+const replacedName = name3.replace('S','M')
+console.log(replacedName)
+
+const anyhow = '    Why do we even need this?     '
+const trimmedAnyhow = anyhow.trim().length
+console.log(trimmedAnyhow)
+
+const abuse = ' joshua is 18 '
+const repeatedAbuse = abuse.repeat(20)
+console.log(repeatedAbuse)
+
+const splitAnyhow = trimmedAnyhow.split('|')
+console.log(splitAnyhow)
